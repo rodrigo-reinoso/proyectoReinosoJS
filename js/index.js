b3cee3ab1f3bc5a622fce52e814e2fe727be920a
Rodrigo Reinoso <rodrigo_cba_15@hotmail.com>
2023-10-16 23:42:19 -0300

    modificando funciones y probando programa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+// Funciones
+
+//mostramos las opciones para que ingrese el usuario atraves de un switch.
 function carrito(opcion) {
   while (opcion !== 0) {
     switch (opcion) {
@@ -13,15 +16,14 @@ function carrito(opcion) {
         quitarDeCarrito();
         break;
 
-      case 4:
-        terminarCompra();
-        break;
+      //case 4:
+      //  terminarCompra();
+      //  break;
 
       default:
         alert("Ingresó un número incorrecto");
         break;
     }
-
     opcion = pedirOpcion();
   }
 
@@ -29,6 +31,7 @@ function carrito(opcion) {
   alert("Gracias por utilizar nuestros servicios.");
 }
 
+//accede a las opciones de producto y agrega los datos en 2 variables total y codigoProducto.
 function agregarAlCarrito() {
   let codigoProducto = pedirProducto();
 
@@ -67,17 +70,7 @@ function agregarAlCarrito() {
   }
 }
 
-function totalResta(monto) {
-  if (monto > total) {
-    alert(
-      "El importe ingresado es mayor al total, por favor ingrese un monto menor."
-    );
-  } else {
-    total = total - monto;
-    alert("El monto quedó en " + total);
-  }
-}
-
+//accede a las opciones de productos y llama a la funcion totalResta a traves de un switch y agrega los importes a restar. 
 function quitarDeCarrito() {
   let codigoProducto = pedirProducto();
 
@@ -111,13 +104,25 @@ function quitarDeCarrito() {
   }
 }
 
+//verifica que el importe no sea mayor que el total que se agrego en carrito.
+function totalResta(monto) {
+  if (monto > total) {
+    alert(
+      "El importe ingresado es mayor al total, por favor ingrese un monto menor."
+    );
+  } else {
+    total = total - monto;
+    alert("El monto quedó en " + total);
+  }
+}
+
+//enviamos el valor total de la compra siempre que sea mayor que 0 sino lo envia nuevamente al menu.
 function valorTotalCarrito() {
-  // enviamos el valor total de la compra
   if (total > 0) {
     alert(
       "Los productos ingresados son: " +
         codigoDeProducto +
-        ", el total de su carrito es de $ " +
+        ", el total de su carrito es de \n $ " +
         total
     );
   } else {
@@ -126,28 +131,31 @@ function valorTotalCarrito() {
   }
 }
 
+//con esta condicion concatenamos el detalle del producto.
 function detalleProducto(codigoProducto) {
-  //con esta condicion concatenamos el detalle del producto
   if (codigoDeProducto !== "") {
     codigoDeProducto += ", ";
   }
   codigoDeProducto += codigoProducto;
 }
 
+//con esta condicion mostramos el saldo a pagar y un saludo, sino lo envia al menu.
 function terminarCompra() {
   if (total > 0) {
-    alert("El saldo a pagar es de : $ " + total);
+    alert("El saldo a pagar es de : \n $" +total);
     alert("Muchas gracias por su compra!");
+    return;
+   
   } else {
     alert("Usted no posee elementos en el carrito.");
-    pedirOpcion();
   }
 }
 
+// estas funciones se utilizan para no repetir codigo -- pedirProducto -- pedirOpcion
 function pedirProducto() {
   return parseInt(
     prompt(
-      "Ingrese el detalle del producto que desee: \n 1. Producto 1 $1000. \n 2. Producto 2 $2000. \n 3. Producto 3 $3000. \n 4. Producto 4 $4000. \n 0. Ingrese 0 para volver al menu anterior. "
+      "Ingrese el detalle del producto que desee: \n 1. Producto 1 $1000. \n 2. Producto 2 $2000. \n 3. Producto 3 $3000. \n 4. Producto 4 $4000. \n 0. Volver al menu anterior. "
     )
   );
 }
@@ -155,7 +163,7 @@ function pedirProducto() {
 function pedirOpcion() {
   return parseInt(
     prompt(
-      "Ingrese la opción que desea: \n 1. Agregar al carrito. \n 2. Detalle total de la compra. \n 3 Eliminar del carrito. \n 4 Finalizar compra. \n 0. Salir  "
+      "Ingrese la opción que desea: \n 1. Agregar al carrito. \n 2. Detalle total de la compra. \n 3 Eliminar del carrito. \n 0. Terminar compra  "
     )
   );
 }
@@ -164,9 +172,11 @@ function pedirOpcion() {
 
 let codigoDeProducto = "";
 let total = 0;
-let totalResta = 0;
+
 
 // Inicio del Programa
 
 let opcion = pedirOpcion();
 carrito(opcion);
+
+// Fin del programa
